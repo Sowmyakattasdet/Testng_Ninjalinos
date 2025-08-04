cd652ecbf0a0dc7f4b51497fe1f066339c1fb1b0
sowmya <sowmya12345g@gmail.com>
2025-08-04 01:05:19 -0500

    home and launch with testng.xml with parallel and crossbrowser

---
+++ july192025/Testng_sowmya/src/test/java/testMethodsPackage/HomeTest.java
@@ -0,0 +1,202 @@
+package testMethodsPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LaunchPage;
+
+public class HomeTest {
+	  WebDriver driver;
+		 LaunchPage Launchpf;
+		 HomePage Homepf;
+		 
+
+			@BeforeMethod
+			void browser()
+			{
+				driver=new FirefoxDriver();
+				//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+				driver.manage().window().maximize();
+				Launchpf = new LaunchPage(driver);
+				Homepf = new HomePage(driver);
+				Launchpf.geturl();
+				driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(300));
+
+				Launchpf.clickgetstarted();
+			}
+		 
+		 //when clicks on Datastructures Getstarted button
+			 @Test(priority=1)
+			    void ClicksDataStBtn()
+			    {
+					Homepf.clickgetstartedDatastructures();
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+		 //Array getstarted button
+			 @Test(priority=2)
+			    void ClickArrayBtn()
+			    {
+				 Homepf.clickgetstartedArray();
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			//LinkedlstGetStartedbtn
+			 @Test(priority=3)
+			    void ClickLinkedlstBtn() 
+			    {
+				 
+				 Homepf.clickgetstartedLinkedList();
+				
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			//StacktGetStartedbtn
+			 @Test(priority=4)
+			    void ClickStacktBtn() 
+			    {
+				 
+				 Homepf.clickgetstartedStack();
+				
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			
+			
+			
+			
+			//QueueGetStartedbtn
+			 @Test(priority=5)
+			    void ClickQueueBtn() throws InterruptedException
+			    {
+				 
+				 Homepf.clickgetstartedQueue();
+				
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			 //TreeGetstartedBtn
+			 @Test(priority=6)
+			    void ClickTreeBtn() 
+			    {
+				 
+				 Homepf.clickgetstartedTree();
+				
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			//GraphGetstartdBtn
+			 @Test(priority=7)
+			    void ClickGraphBtn() 
+			    {
+				 
+				 Homepf.clickgetstartedGraph();
+				
+					String actmsg = Homepf.notloggedinmesg();
+					String expmsg = "You are not logged in";
+					Assert.assertEquals(actmsg, expmsg,
+							"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			    }
+			//DropdownDS
+			@Test(priority=8)
+			void DropdownDS()
+			{
+				Homepf.clickDropdownbox();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=9)
+			void DropdownArray()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownArrays();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=10)
+			void DropdownLinkedlst()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownLinkedList();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=11)
+			void DropdownStack()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownStack();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=12)
+			void DropdownQueue()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownQueue();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=13)
+			void DropdownTree()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownTree();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=14)
+			void DropdownGraph()
+			{
+				Homepf.clickDropdownbox();
+				Homepf.clickDropdownGraph();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+			}
+			@Test(priority=15)
+			 void clickregister() {
+				Homepf.clickRegister();
+			}
+
+//		 
+//			 @AfterMethod
+//			    void quitbrowser()
+//			    {
+//			    	Launchpf.quitdriver();
+//			    }	 
+//		 
+}
--- july192025/Testng_sowmya/src/test/java/testMethodsPackage/LaunchTest.java
+++ july192025/Testng_sowmya/src/test/java/testMethodsPackage/LaunchTest.java
@@ -6,13 +6,15 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pageObjects.LaunchPage;
@@ -27,9 +29,17 @@ public class LaunchTest {
 	
 	
 	@BeforeMethod
-	void browser()
+	@Parameters({"browser"})
+	void browser(String br)
 	{
-		driver=new ChromeDriver();
+		switch(br.toLowerCase())
+		{
+		case "chrome": driver=new ChromeDriver();break;
+		case "firefox": driver=new FirefoxDriver();break;
+		case "edge": driver=new EdgeDriver();break;
+		default: System.out.println("Invalid browser");return;
+		}
+		//driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		Launchpf = new LaunchPage(driver);
@@ -52,7 +62,7 @@ public class LaunchTest {
 		Assert.assertEquals(actmsg, expmsg, "user is not on the homepage with title numpyninja");
     }
     @AfterMethod
-    void quitbrowser()
+    void tearDown()
     {
     	Launchpf.quitdriver();
     }
