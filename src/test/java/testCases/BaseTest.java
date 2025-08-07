e35d53ecd20f22b5bc411a45fef787c9db338b96
JayasriVijay <savithri87@gmail.com>
2025-08-07 00:22:58 -0400

    changed code for screenshot

--- src/test/java/testCases/BaseTest.java
+++ src/test/java/testCases/BaseTest.java
@@ -1,7 +1,10 @@
 package testCases;
 
 import java.io.File;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -13,6 +16,7 @@ import org.testng.annotations.Parameters;
 import com.aventstack.chaintest.plugins.ChainTestListener;
 
 import driverFactory.DriverFactory_TestNG;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import pageFactory.BasePage;
 
@@ -40,14 +44,25 @@ public class BaseTest {
 
 	}
 	
-	@Attachment(value = "Screenshot", type = "image/jpg")
-	public byte[] failed_screenshot(String testMethodName) throws IOException {
+	//allure generate --clean target/allure-results -o target/allure-report
+	//allure open target/allure-report
+	//@Attachment(value = "Screenshot", type = "image/jpg")
+	public void failed_screenshot(String testMethodName) throws IOException {
 		
-		byte[] screenshot_for_allureReport = ((TakesScreenshot)DriverFactory_TestNG.getDriver()).getScreenshotAs(OutputType.BYTES);
+		//byte[] screenshot_for_allureReport = ((TakesScreenshot)DriverFactory_TestNG.getDriver()).getScreenshotAs(OutputType.BYTES);
 		File screenshot = ((TakesScreenshot)DriverFactory_TestNG.getDriver()).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshot, new File("target/screenshots/"+"screenshot_"+testMethodName+".jpg"));
-		ChainTestListener.embed(screenshot, "image/jpg");
-		return screenshot_for_allureReport;
+		File savedScreenshot = new File("target/screenshots/"+"screenshot_"+testMethodName+".jpg");
+		FileUtils.copyFile(screenshot, savedScreenshot);
+		try (InputStream is = new FileInputStream(savedScreenshot)) {
+	        Allure.addAttachment("Screenshot", "image/jpg", is, ".jpg");
+	    }
+		ChainTestListener.embed(savedScreenshot, "image/jpg");
+		//return screenshot_for_allureReport;
 	}
-
 }
+
+		
+		
+	
+
+
